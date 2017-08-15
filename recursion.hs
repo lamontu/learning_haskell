6e758f5795ecf61faadd2b0ea820c96af439ceca
lamontu <lamontyu@163.com>
2017-08-16 07:34:52 +0800

    Test tuples using colour points

--- recursion.hs
+++ recursion.hs
@@ -1,5 +1,33 @@
 import Data.Char
 
+type Point = (Int, Int)
+orgin :: Point
+orgin = (0, 0)
+-- move a given point to the right
+moveRight :: Point -> Int -> Point
+moveRight (x, y) distance = (x + distance, y)
+-- move a given point to upwards
+moveUp :: Point -> Int -> Point
+moveUp (x, y) distance = (x, y + distance)
+
+type Colour = String
+type ColourPoint = (Int, Int, Colour)
+origin :: Colour -> ColourPoint
+origin colour = (0, 0, colour)
+
+-- move a colour point vertically and horizontally
+move :: ColourPoint -> Int -> Int -> ColourPoint
+move (x, y, colour) xDistance yDistance
+  = (x + xDistance, y + yDistance, colour)
+
+-- compute the distance between two colour points
+distance :: ColourPoint -> ColourPoint -> Float
+distance (x1, y1, colour1) (x2, y2, colour2)
+  = sqrt (fromIntegral (dx * dx + dy * dy))
+  where
+    dx = x2 - x1
+    dy = y2 - y1
+
 natSum :: (Num a, Ord a) => a -> a
 natSum 0 = 0
 natSum n | n > 0 = n + natSum (n - 1)
