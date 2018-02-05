cfd66f550ac15d02e46c85049eafc1ae2263f2d1
lamontu <lamontyu@163.com>
2018-02-05 09:53:58 +0800

    Test random

---
+++ random.hs
@@ -0,0 +1,17 @@
+import System.Random
+
+threeCoins :: StdGen -> (Bool, Bool, Bool)
+threeCoins gen =
+    let (firstCoin, newGen) = random gen
+        (secondCoin, newGen') = random newGen
+        (thirdCoin, newGen'') = random newGen'
+    in (firstCoin, secondCoin, thirdCoin)
+
+lst = take 5 $ randoms (mkStdGen 12) :: [Int]
+
+finiteRandoms :: (RandomGen g, Random a, Num n, Eq n) => n -> g -> ([a], g)
+finiteRandoms 0 gen = ([], gen)
+finiteRandoms n gen = 
+    let (value, newGen) = random gen
+        (restOfList, finalGen) = finiteRandoms (n-1) newGen
+    in (value:restOfList, finalGen)
