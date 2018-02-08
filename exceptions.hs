c7027bb4ee60294822bd2c575923bd940750b80a
lamontu <lamontyu@163.com>
2018-02-08 17:27:28 +0800

    Test IO exception

---
+++ exceptions.hs
@@ -0,0 +1,16 @@
+import System.Environment
+import System.IO
+-- import System.IO.Error
+import Control.Exception
+
+-- main = toTry `catchIOError` handler
+main = toTry `catch` handler
+
+toTry :: IO ()
+toTry = do
+    (fileName:_) <- getArgs
+    contents <- readFile fileName
+    putStrLn $ "The file has " ++ show (length (lines contents))++" lines!"
+
+handler :: IOError -> IO ()
+handler e = putStrLn "Whoops, had some trouble!"
