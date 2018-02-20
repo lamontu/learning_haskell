428f22b020417049238d28ef0f0fcd0d36ce32ca
lamontu <lamontyu@163.com>
2018-02-21 00:28:14 +0800

    Test keyword newtype

---
+++ new_type.hs
@@ -0,0 +1,11 @@
+newtype CharList = CharList { getCharList :: [Char] } deriving (Eq, Show)
+
+newtype Pair b a = Pair { getPair :: (a,b) }
+
+instance Functor (Pair c) where
+    fmap f (Pair (x,y)) = Pair (f x, y)
+
+-- data CoolBool = CoolBool { getCoolBool :: Bool }
+newtype CoolBool = CoolBool { getCoolBool :: Bool }
+helloMe :: CoolBool -> String
+helloMe (CoolBool _) = "hello"
