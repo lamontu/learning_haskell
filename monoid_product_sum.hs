70c642939842d8b503d7c799345713b381a80c4c
lamontu <lamontyu@163.com>
2018-02-21 18:32:01 +0800

    Create product and sum monoid

---
+++ monoid_product_sum.hs
@@ -0,0 +1,20 @@
+-- class Monoid m where
+--     mempty :: m
+--     mappend :: m -> m -> m
+--     mconcat :: [m] -> m
+--     mconcat = foldr mappend mempty
+
+newtype Product a = Product {getProduct :: a}
+    deriving (Eq, Ord, Read, Show, Bounded)
+
+instance Num a => Monoid (Product a) where
+    mempty = Product 1
+    Product x `mappend` Product y = Product (x * y)
+
+
+newtype Sum a = Sum {getSum :: a}
+    deriving (Eq, Ord, Read, Show, Bounded)
+
+instance Num a => Monoid (Sum a) where
+    mempty = Sum 0
+    Sum x `mappend` Sum y = Sum (x + y)
