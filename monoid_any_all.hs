efd24b0f8c71a5c0cfd015137428a5d45f045e9d
lamontu <lamontyu@163.com>
2018-02-21 22:14:24 +0800

    Create any and all monoid

---
+++ monoid_any_all.hs
@@ -0,0 +1,14 @@
+newtype Any = Any {getAny :: Bool}
+    deriving (Eq, Ord, Read, Show, Bounded)
+
+instance Monoid Any where
+    mempty = Any False
+    Any x `mappend` Any y = Any (x || y)
+
+
+newtype All = All {getAll :: Bool}
+    deriving (Eq, Ord, Read, Show, Bounded)
+
+instance Monoid All where
+    mempty = All True
+    All x `mappend` All y = All (x && y)
